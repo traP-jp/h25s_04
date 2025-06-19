c975340324421e9033f3b12ce77cf03d437732fb
Pugma <maronmarondelicious@gmail.com>
2025-06-19 20:52:53 +0900

    client-dev: installed linter & formatter

---
+++ client/prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  semi: false,
+}
+
+export default config
