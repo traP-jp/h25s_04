84633db4fc71e2fd3aabbb21241e47cec6fb18c8
Pugma <maronmarondelicious@gmail.com>
2025-06-22 07:54:42 +0900

    feat: floating list

---
+++ client/src/components/EateryList.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+// import type { Eatery } from '../lib/apis'
+
+// interface Props {
+//   eateries: Eatery[]
+// }
+
+const eateries = [
+  { name: 'Test Eatery', description: 'This is a test eatery', id: 1 },
+  { name: 'Sample Eatery', description: 'This is a sample eatery', id: 2 },
+  { name: 'Demo Eatery', description: 'This is a demo eatery', id: 3 },
+]
+
+// const { eateries } = defineProps<Props>()
+</script>
+
+<template>
+  <ul>
+    <li v-for="eatery in eateries" :key="eatery.id">
+      {{ eatery.name }} {{ eatery.description }} a
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" module></style>
--- client/src/components/EateryMap.vue
+++ client/src/components/EateryMap.vue
@@ -2,6 +2,19 @@
 import 'leaflet/dist/leaflet.css'
 import { ref } from 'vue'
 import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+import apis, { type Eatery } from '../lib/apis'
+import EateryList from '../components/EateryList.vue'
+
+const eateries = ref<Eatery[]>([])
+apis
+  .eateriesGet()
+  .then((res) => {
+    eateries.value = res.data.data ?? []
+    console.log(res.data)
+  })
+  .catch((error) => {
+    console.error('Error fetching eateries:', error)
+  })
 
 const zoom = ref(16)
 const center = ref([35.605958, 139.68354]) // 地図の中心座標
@@ -14,6 +27,9 @@ const markerPosition = ref([35.605958, 139.68354]) // ピンを立てる座標
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <l-marker :lat-lng="markerPosition"></l-marker>
     </l-map>
+    <div :class="$style.list">
+      <eatery-list :eateries="eateries" />
+    </div>
   </div>
 </template>
 
@@ -21,5 +37,17 @@ const markerPosition = ref([35.605958, 139.68354]) // ピンを立てる座標
 .map {
   height: min(100vh, 800px);
   width: 100%;
+  position: relative;
+}
+.list {
+  color: black;
+  position: absolute; /* 絶対位置に変更 */
+  z-index: 1000; /* 地図より前面に表示 */
+  top: 100px; /* 必要に応じて調整 */
+  left: 20px; /* 必要に応じて調整 */
+  background-color: rgba(255, 255, 255, 1);
+  padding: 16px;
+  border-radius: 12px;
+  box-shadow: 16px 16px 20px rgba(0, 0, 0, 0.2);
 }
 </style>
