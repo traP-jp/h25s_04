1e80ef62f585bc23826553a6a2ad66de27403090
Pugma <maronmarondelicious@gmail.com>
2025-06-21 11:01:29 +0900

    feat: map 実装してみた

---
+++ client/src/components/EateryMap.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css'
+import { ref } from 'vue'
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+
+const zoom = ref(16)
+const center = ref([35.605958, 139.68354]) // 地図の中心座標
+const markerPosition = ref([35.605958, 139.68354]) // ピンを立てる座標
+</script>
+
+<template>
+  <div style="height: 600px; width: 800px">
+    <l-map v-model:zoom="zoom" :use-global-leaflet="false" :center="center">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-marker :lat-lng="markerPosition"></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<style lang="scss" module>
+/* 必要に応じてスタイルを追加 */
+</style>
