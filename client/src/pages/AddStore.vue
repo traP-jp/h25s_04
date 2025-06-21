8853d5b443b176d38b112535f3b9582cb430891d
msk <masaki181206@gmail.com>
2025-06-21 17:12:11 +0900

    店の追加ページの入力フォームを整えました。

--- client/src/pages/AddStore.vue
+++ client/src/pages/AddStore.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="honbun">
     <div>
-     <span style="color: #000000;">店名</span> <input type="text" v-model="message" placeholder="入力"><br>
-     <span style="color: #000000;">説明</span> <input type="text" v-model="message" placeholder="入力"><br>
-     <input type="text" v-model="message" placeholder="入力"><br>
-     <span style="color: #000000;">住所／座標</span> <input type="text" v-model="message" placeholder="入力"><br>
-     <span style="color: #000000;">レビュー本文</span> <textarea v-model="inputText" rows="5" placeholder="入力"></textarea>
-     </div>
+     <span style="color: #000000;">店名</span> <input type="text" style="color: #000000; background-color: #FFFFFF; border: 1px solid #B2BBC7; border-radius: 3px;" v-model="message" placeholder="店名を入力"><br>
+     <span style="color: #000000;">説明</span> <input type="text" style="color: #000000; background-color: #FFFFFF; border: 1px solid #B2BBC7; border-radius: 3px" v-model="message" placeholder="営業日を入力"><br>
+     <input type="text" style="color: #000000; background-color: #FFFFFF; border: 1px solid #B2BBC7; border-radius: 3px" v-model="message" placeholder="営業時間を入力"><br>
+     <span style="color: #000000;">住所／座標</span> <input type="text"  style="color: #000000; background-color: #FFFFFF; border: 1px solid #B2BBC7; border-radius: 3px" v-model="message" placeholder="住所／座標を入力"><br>
+     <span style="color: #000000;">レビュー本文</span> <textarea v-model="inputText" rows="5"  style="color: #000000; background-color: #FFFFFF; border: 1px solid #B2BBC7; border-radius: 3px" placeholder="レビューを入力"></textarea><br>
+     <span style="color: #000000; ">お店の写真</span><input type="file" accept="image/*" style="border: 1px solid #B2BBC7; border-radius: 3px;">
+   </div>
    <div>
     <post-button></post-button>
    </div>
