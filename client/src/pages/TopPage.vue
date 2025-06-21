1b2062f28bebc5803eb7bd168d630d9a22186406
o_o <sksky30030@gmail.com>
2025-06-21 16:17:05 +0900

    なおしました

--- client/src/pages/TopPage.vue
+++ client/src/pages/TopPage.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import EateryTile from '../components/EateryTile.vue';
+
+</script>
+
 <template>
   <main><h1>TopPage</h1></main>
+  <EateryTile></EateryTile>
 </template>
 
 <style lang="scss" module></style>
