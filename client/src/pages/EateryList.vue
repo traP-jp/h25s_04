0eb4dca37a9f120e82e10cca234dbd12ac891bc1
Pugma <maronmarondelicious@gmail.com>
2025-06-21 11:04:25 +0900

    page: eatery list

---
+++ client/src/pages/EateryList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import EateryMap from '../components/EateryMap.vue'
+</script>
+
+<template>
+  <eatery-map></eatery-map>
+</template>
+
+<style lang="scss" module></style>
