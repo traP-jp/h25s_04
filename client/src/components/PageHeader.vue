832c4224c756539a1e1f541bff09d43998d1003b
Pugma <maronmarondelicious@gmail.com>
2025-06-21 09:47:44 +0900

    component: header

---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header :class="$style.header">
+    <div>aaaa</div>
+    <div>bbbb</div>
+  </header>
+</template>
+
+<style module>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px;
+}
+</style>
