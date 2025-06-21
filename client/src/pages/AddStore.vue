7950fe2895938a9acf6b2e5d8fe90d985d45c9ea
msk <masaki181206@gmail.com>
2025-06-21 17:51:48 +0900

    店追加ページを修正しました

--- client/src/pages/AddStore.vue
+++ client/src/pages/AddStore.vue
@@ -1,66 +1,51 @@
 <template>
   <div class="honbun">
     <div>
-      <span style="color: #000000">店名</span>
-      <input
-        v-model="message"
-        type="text"
-        style="
-          color: #000000;
-          background-color: #ffffff;
-          border: 1px solid #b2bbc7;
-          border-radius: 3px;
-        "
-        placeholder="店名を入力"
-      /><br />
-      <span style="color: #000000">説明</span>
-      <input
-        v-model="message"
-        type="text"
-        style="
-          color: #000000;
-          background-color: #ffffff;
-          border: 1px solid #b2bbc7;
-          border-radius: 3px;
-        "
-        placeholder="営業日を入力"
-      /><br />
-      <input
-        v-model="message"
-        type="text"
-        style="
-          color: #000000;
-          background-color: #ffffff;
-          border: 1px solid #b2bbc7;
-          border-radius: 3px;
-        "
-        placeholder="営業時間を入力"
-      /><br />
-      <span style="color: #000000">住所／座標</span>
-      <input
-        v-model="message"
-        type="text"
-        style="
-          color: #000000;
-          background-color: #ffffff;
-          border: 1px solid #b2bbc7;
-          border-radius: 3px;
-        "
-        placeholder="住所／座標を入力"
-      /><br />
-      <span style="color: #000000">レビュー本文</span>
-      <textarea
-        v-model="inputText"
-        rows="5"
-        style="
-          color: #000000;
-          background-color: #ffffff;
-          border: 1px solid #b2bbc7;
-          border-radius: 3px;
-        "
-        placeholder="レビューを入力"
-      ></textarea
-      ><br />
+      <div>
+       <span style="color: #000000">店名</span>
+       <input
+         v-model="message1"
+         class="nyuuryoku4"
+         type="text"
+         placeholder="入力"
+       />
+      </div>
+      <div>
+       <span style="color: #000000">説明</span>
+       <input
+         v-model="message2"
+         class="nyuuryoku4"
+         type="text"
+         placeholder="入力"
+       />
+      </div>
+      <div>
+       <input
+         v-model="message3"
+         class="nyuuryoku4"
+         type="text"
+         placeholder="入力"
+       />
+      </div>
+      <div>
+       <span style="color: #000000">住所／座標</span>
+       <input
+         v-model="message4"
+         class="nyuuryoku4"
+         type="text"
+         placeholder="入力"
+       />
+      </div>
+      <div>
+       <span style="color: #000000">レビュー本文</span>
+       <textarea
+         v-model="inputText"
+         class="nyuuryoku4"
+         rows="5"
+         placeholder="入力"
+       ></textarea
+       >
+      </div>
       <span style="color: #000000">お店の写真</span
       ><input
         type="file"
@@ -76,13 +61,23 @@
 
 <script lang="ts" setup>
 import PostButton from './PostButton.vue'
+import { ref } from 'vue'
 
-const message = ''
-const inputText = ''
+const message1 = ref('')
+const message2 = ref('')
+const message3 = ref('')
+const message4 = ref('')
+const inputText = ref('')
 </script>
 
 <style>
 .honbun {
   background-color: #ffffff;
+}
+.nyuuryoku4{
+  color: #000000;
+  background-color: #ffffff;
+  border: 1px solid #b2bbc7;
+  border-radius: 3px;
 }
 </style>
