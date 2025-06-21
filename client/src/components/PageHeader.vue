086c1ac416c94175531459ca6c36ec35c10e382f
Pugma <maronmarondelicious@gmail.com>
2025-06-21 22:20:16 +0900

    refactor: header

--- client/src/components/PageHeader.vue
+++ client/src/components/PageHeader.vue
@@ -4,13 +4,17 @@ import PrimaryButton from './PrimaryButton.vue'
 
 <template>
   <header :class="$style.header">
-    <RouterLink to="/"><div>aaaa</div></RouterLink>
+    <RouterLink to="/">
+      <div>ここにロゴを入れる</div>
+    </RouterLink>
     <div>タイトル</div>
-    <div class="buttons">
-      <RouterLink to="/eatery"><PrimaryButton :text="'お店一覧'" /></RouterLink>
-      <RouterLink to="/eatery/new"
-        ><PrimaryButton :text="'新規投稿'"
-      /></RouterLink>
+    <div :class="$style.buttons">
+      <RouterLink to="/eatery">
+        <PrimaryButton :text="'お店一覧'" />
+      </RouterLink>
+      <RouterLink to="/eatery/new">
+        <PrimaryButton :text="'新規投稿'" />
+      </RouterLink>
     </div>
   </header>
 </template>
@@ -18,9 +22,15 @@ import PrimaryButton from './PrimaryButton.vue'
 <style lang="scss" module>
 .header {
   display: flex;
+  height: 75px;
   background-color: #ffad3a;
   justify-content: space-between;
   align-items: center;
   padding: 8px;
+}
+
+.buttons {
+  display: flex;
+  gap: 8px;
 }
 </style>
