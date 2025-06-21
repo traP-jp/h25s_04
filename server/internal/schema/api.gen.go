61f969231ce45851a7970f5c1660a32d20ca50ed
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-21 23:51:48 +0900

    レスポンスのスキーマが直書きされていたのを修正

--- server/internal/schema/api.gen.go
+++ server/internal/schema/api.gen.go
@@ -52,6 +52,12 @@ type EateryCreate struct {
 	Name string `json:"name"`
 }
 
+// EateryListResponse defines model for EateryListResponse.
+type EateryListResponse struct {
+	Data       []Eatery   `json:"data"`
+	Pagination Pagination `json:"pagination"`
+}
+
 // EateryUpdate defines model for EateryUpdate.
 type EateryUpdate struct {
 	// Description Description of the eatery
@@ -134,6 +140,12 @@ type ReviewDetail struct {
 
 	// UpdatedAt Last update timestamp
 	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// ReviewListResponse defines model for ReviewListResponse.
+type ReviewListResponse struct {
+	Data       []ReviewSummary `json:"data"`
+	Pagination Pagination      `json:"pagination"`
 }
 
 // ReviewSummary Summary of a review for list views (without full content)
