951399ad202a2b8189c0648ac046e16153f295ad
msk <masaki181206@gmail.com>
2025-06-21 13:54:48 +0900

    ヘッダーのボタンを作りました

--- client/src/components/PageHeader.vue
+++ client/src/components/PageHeader.vue
@@ -1,22 +1,23 @@
 <script lang="ts" setup>
-import PrimaryButton from "./PrimaryButton.vue"
-import SecondaryButton from "./SecondaryButton.vue"
+import PrimaryButton from './PrimaryButton.vue'
+import SecondaryButton from './SecondaryButton.vue'
 </script>
 
 <template>
   <header :class="$style.header">
     <div>aaaa</div>
     <div>タイトル</div>
-    <primary-button></primary-button>
-    <secondary-button></secondary-button>
-
+    <div class="buttons">
+      <primary-button></primary-button>
+      <secondary-button></secondary-button>
+    </div>
   </header>
 </template>
 
 <style lang="scss" module>
 .header {
   display: flex;
-  background-color: #FFAD3A;
+  background-color: #ffad3a;
   justify-content: space-between;
   align-items: center;
   padding: 8px;
--- client/src/components/PrimaryButton.vue
+++ client/src/components/PrimaryButton.vue
@@ -1,14 +1,11 @@
 <template>
-  <button class="omise">
-    お店一覧
-  </button>
+  <button class="omise">お店一覧</button>
 </template>
 
 <style lang="scss" modele>
-.omise{
-    font-family: M PULS 1p;
-    font-weight: 1000;
-    border: 1px solid $color-secondary;
-    background-color: #FFAD3A;
+.omise {
+  font-weight: 1000;
+  border: 1px solid $color-secondary;
+  background-color: #ffad3a;
 }
 </style>
