ec5633f135fc5d3330629b90bb33160915bca2cd
kyoupie <takahashi.k.dd93@m.isct.ac.jp>
2025-06-21 16:28:40 +0900

    UploadImageを導入

--- server/internal/repository/image.go
+++ server/internal/repository/image.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"io"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/google/uuid"
 )
 
@@ -17,5 +19,17 @@ type (
 func (r *Repository) UploadImage(ctx context.Context, image io.Reader) (uuid.UUID, error) {
 	imageID := uuid.New()
 	// オブジェクトストレージに画像をアップロードする
+	if _, err := r.client.PutObject(
+		ctx,
+		&s3.PutObjectInput{
+			Bucket:      aws.String("leaq"),
+			Key:         aws.String(imageID.String()),
+			Body:        image,
+			ContentType: aws.String("image/png"),
+		},
+	); err != nil {
+		return uuid.Nil, err
+	}
+
 	return imageID, nil
 }
