47c85e67b2200c9d5fb440ddf0ab3b039e57f930
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-22 12:05:42 +0900

    traQにメッセージを投稿できるようにするための準備

--- server/internal/handler/handler.go
+++ server/internal/handler/handler.go
@@ -1,16 +1,26 @@
 package handler
 
 import (
+	"context"
+
+	"github.com/traPtitech/go-traq"
+
 	"github.com/traP-jp/h25s_04/server/internal/repository"
+	"github.com/traP-jp/h25s_04/server/pkg/config"
 )
 
 type Handler struct {
-	repo *repository.Repository
+	repo       *repository.Repository
+	traqClient *traq.APIClient
+	traqAuth   context.Context
 }
 
 func New(repo *repository.Repository) *Handler {
+	client, auth := config.Traq()
 	return &Handler{
-		repo: repo,
+		repo:       repo,
+		traqClient: client,
+		traqAuth:   auth,
 	}
 }
 
