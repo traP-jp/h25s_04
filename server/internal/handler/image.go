94d4f287c0b329b23ec948c46fd21fcb3e72c698
ch-eeze <aphrodite.venus.436@gmail.com>
2025-06-22 09:22:48 +0900

    変数名の修正とファイル形式の検出をできるようにした

--- server/internal/handler/image.go
+++ server/internal/handler/image.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"net/http"
 
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	"github.com/oapi-codegen/runtime/types"
 
@@ -23,14 +25,13 @@ func (h *Handler) PostImages(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "bad request")
 	}
 
-	UploadImage := http.DetectContentType(b)
-	if UploadImage != "image/" {
+	contentType := http.DetectContentType(b)
+	if !strings.HasPrefix(contentType, "image/") {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid file format")
 	}
 
-
 	reader := bytes.NewReader(b)
-	imageID, err := h.repo.UploadImage(c.Request().Context(), reader, UploadImage)
+	imageID, err := h.repo.UploadImage(c.Request().Context(), reader, contentType)
 
 	if err != nil {
 		c.Logger().Errorf("failed to upload image: %v", err)
